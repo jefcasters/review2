7e069b09cc8d753ec23ddd86bd4369e0abe1f05a
jef <jef_casters@hotmail.com>
2014-12-31 15:16:26 -0500

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = true
   ActionMailer::Base.smtp_settings ={
     :address              => "just112.justhost.com",
     :port                 => 465,
